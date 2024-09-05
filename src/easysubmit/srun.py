881cd0e41f17dd56eef819d9a34dfdc8e23ebd1c
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-09-05 16:09:54 -0400

    Refactor import statements and update SLURMConfig output and error paths

--- src/easysubmit/srun.py
+++ src/easysubmit/srun.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-from base import Scheduler
-from slurm import SLURMCluster, SLURMConfig
+from easysubmit.base import Scheduler
+from easysubmit.slurm import SLURMCluster, SLURMConfig
 
 
 def worker_func(config: dict):
@@ -10,17 +10,19 @@ def worker_func(config: dict):
 
 def main():
     config = SLURMConfig(
-        partition="gpuq",  # contrib-gpuq
+        partition="contrib-gpuq",  # contrib-gpuq
         qos="gpu",
         nodes=1,
         ntasks_per_node=1,
         gres="gpu:3g.40gb:1",
         mem="32G",
-        output="logs/slurm/job-%x-%A/task-%a-%N.out",
-        error="logs/slurm/job-%x-%A/task-%a-%N.err",
+        output="{LOGS_DIR}/job-%j-slurm-%x-%A_%a-%N.out",
+        error="{LOGS_DIR}/job-%j-slurm-%x-%A_%a-%N.err",
     )
     cluster = SLURMCluster(config)
     scheduler = Scheduler(cluster, worker_func)
+    scheduler.configs["A"] = {"name": "A"}
+    scheduler.configs["B"] = {"name": "B"}
     scheduler.run()
 
 
