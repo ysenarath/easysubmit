ca28998c3f376e00b6a3b211095254b0ca559fab
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-09-05 18:02:35 -0400

    Add new task to scheduler

---
+++ examples/tasks_1.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import time
+
+from easysubmit.base import Scheduler, Task
+from easysubmit.slurm import SLURMCluster, SLURMConfig
+
+
+class ExampleTask(Task):
+    def run(self):
+        time.sleep(20)
+        print(f"Running task {self.id} with config {self.config}")
+
+
+def main():
+    config = SLURMConfig(
+        partition="contrib-gpuq",  # contrib-gpuq
+        qos="gpu",
+        nodes=1,
+        ntasks_per_node=1,
+        gres="gpu:3g.40gb:1",
+        mem="32G",
+        output="{LOGS_DIR}/job-%j-slurm-%x-%A_%a-%N.out",
+        error="{LOGS_DIR}/job-%j-slurm-%x-%A_%a-%N.err",
+    )
+    cluster = SLURMCluster(config)
+    scheduler = Scheduler(cluster)
+    scheduler.tasks.append(ExampleTask("task_a", {"name": "Alice"}))
+    scheduler.tasks.append(ExampleTask("task_b", {"name": "Bob"}))
+    scheduler.run()
+
+
+if __name__ == "__main__":
+    main()
