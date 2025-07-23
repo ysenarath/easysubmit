83d899803bbace0496217bcf4a4821eac32e78e6
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-07-23 01:25:26 -0400

    Bump version to 0.2.2

--- src/easysubmit/__init__.py
+++ src/easysubmit/__init__.py
@@ -1,7 +1,7 @@
 from easysubmit.entities import Cluster, Job, Task, TaskConfig, AutoTask
 from easysubmit.slurm import SLURMCluster, SLURMConfig
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 __all__ = [
     "Task",
