8a33b6ffad5a6e5da42eeeee16aedce0f0d2eaa4
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2026-09-07 12:10:34 -0400

    Bump version to 0.4.0 for breaking task API changes

--- src/easysubmit/__init__.py
+++ src/easysubmit/__init__.py
@@ -3,7 +3,7 @@ from easysubmit.slurm import SLURMCluster, SLURMConfig
 from easysubmit.functions import FunctionExecutor
 from easysubmit.base import schedule
 
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 
 __all__ = [
     "Task",
